6c45069f1850584da28c668a68c266dfd5cd7d5d
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-07-23 17:10:51 -0300

    feat:task item

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,7 +1,10 @@
 import { Container } from "../components/container";
+import { TaskItem } from "../components/core/task-item";
+import { TasksSummary } from "../components/core/tasks-summary";
 
 export function PageHome() {
-  return <Container>
-    
+  return <Container className="space-y-3">
+    <TasksSummary/>
+    <TaskItem/>
   </Container>;
 }
